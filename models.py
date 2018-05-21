dc9c11fc35ce10db866514c2c4a4437cbf8cadd3
Thomas-95 <thomass4695@gmail.com>
2018-05-21 21:48:43 +0100

    Populate  file.

--- models.py
+++ models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+class Article(models.Model):
+    article_title = models.CharField(max_length=100)
+    article_text = models.TextField()
+    pub_date = models.DateTimeField('Date Published')
+
+    def __str__(self):
+        return self.article_title
+
+    def count_article_comments(self):
+        return self.comments.count()
+
+
+class Comment(models.Model):
+    article = models.ForeignKey('blog.Article', on_delete=models.CASCADE, related_name='comments')
+    author = models.CharField(max_length=200)
+    post_date = models.DateTimeField(default=timezone.now)
+    text = models.TextField()
+
+    def __str__(self):
+        return self.text
+
